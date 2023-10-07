35e757e55ed361bb6222d60b7f7686ab6d4403fc
loganamcnichols@gmail.com <loganamcnichols@gmail.com>
2023-10-06 19:36:53 -0700

    Added a test to find cases from document numbers.

--- integration_tests/login_test.go
+++ integration_tests/login_test.go
@@ -17,7 +17,7 @@ func TestLoginToPacer(t *testing.T) {
 		t.Fatalf("url.Parse() returned error: %v", err)
 	}
 	cookies := client.Jar.Cookies(u)
-	cookieName := "nextGenCSO"
+	cookieName := "NextGenCSO"
 	for _, cookie := range cookies {
 		if cookie.Name == cookieName {
 			return
@@ -25,4 +25,21 @@ func TestLoginToPacer(t *testing.T) {
 	}
 	t.Fatalf("LoginToPacer() did not return a %s cookie", cookieName)
 
+}
+
+func TestSearchByDocketNumber(t *testing.T) {
+	client, _ := scraper.LoginToPacer()
+	data, err := scraper.SearchByDocketNumber(client, "https://ecf.azd.uscourts.gov/cgi-bin/possible_case_numbers.pl?22-02189")
+	if err != nil {
+		t.Fatalf("SearchByDocketNumber() returned error: %v", err)
+	}
+	if data.Number != "22-02189" {
+		t.Fatalf("SearchByDocketNumber() returned incorrect docket number: %s", data.Number)
+	}
+	if len(data.Cases) != 4 {
+		t.Fatalf("SearchByDocketNumber() returned incorrect number of cases: %d", len(data.Cases))
+	}
+	if data.Cases[2].ID != "1313500" {
+		t.Fatalf("SearchByDocketNumber() returned incorrect case ID: %s", data.Cases[2].ID)
+	}
 }
