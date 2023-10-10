18f8c35fc51187536e50cc47ba5044e0d020dece
loganamcnichols <131667269+loganamcnichols@users.noreply.github.com>
2023-10-10 13:31:06 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,3 +4,5 @@ CREATE TABLE cases(
   court_id VARCHAR(4),
   title TEXT
   );
+
+INSERT INTO cases (pacer_id, court_id, title) VALUES (1320666, 'azd', '2:22-cv-02189-SRB Stanley v. Quintairos Prieto Wood & Boyer PA');
